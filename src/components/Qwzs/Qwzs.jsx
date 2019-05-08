f05949f9e37c3a815e066a8896177d8cfee788c2
b159732000 <b159732000@users.noreply.github.com>
2019-05-08 15:42:53 +0800

    區位展示的layout做好了
    準備開始做這一頁的動畫

--- src/components/Qwzs/Qwzs.jsx
+++ src/components/Qwzs/Qwzs.jsx
@@ -1,16 +1,117 @@
 import React from 'react';
-import './Qwzs.css';
+import './Qwzs.scss';
 
 class Qwzs extends React.Component {
     render() {
         return (
-            <div>
-            
-                <div>This Qwzs, Qwzs import successed.</div>
+            <div className="QwzsContainer">
+
+                {/* 背景鏤空字 */}
+                <div className="bgStrokeText">LOCATION</div>
+
+                {/* 四大灣區...等等共五個清單 */}
+                <div className="choiceList">
+                    <ul>
+                        <li>
+                            <div className="imgDiv">
+                                <img src={require('../../images/DA_08-995x560.jpg')} alt="" />
+                            </div>
+                            <div className="content">
+                                <div className="title">四大灣區</div>
+                                <div className="instruction">
+                                    <div>Lorem Ipsum dummy</div>
+                                    <div>Text of the printing industry.</div>
+                                    <div>2019</div>
+                                </div>
+                                <div className="bottom">
+                                    <div className="currentPage">01</div>
+                                    <div className="slash">/</div>
+                                    <div className="totalPages">15</div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div className="imgDiv">
+                                <img src={require('../../images/MS_06-copy-995x560.jpg')} alt="" />
+                            </div>
+                            <div className="content">
+                                <div className="title">四大灣區</div>
+                                <div className="instruction">
+                                    <div>Lorem Ipsum dummy</div>
+                                    <div>Text of the printing industry.</div>
+                                    <div>2019</div>
+                                </div>
+                                <div className="bottom">
+                                    <div className="currentPage">01</div>
+                                    <div className="slash">/</div>
+                                    <div className="totalPages">15</div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div className="imgDiv">
+                                <img src={require('../../images/AGT_S13_05-995x560.jpg')} alt="" />
+                            </div>
+                            <div className="content">
+                                <div className="title">四大灣區</div>
+                                <div className="instruction">
+                                    <div>Lorem Ipsum dummy</div>
+                                    <div>Text of the printing industry.</div>
+                                    <div>2019</div>
+                                </div>
+                                <div className="bottom">
+                                    <div className="currentPage">01</div>
+                                    <div className="slash">/</div>
+                                    <div className="totalPages">15</div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div className="imgDiv">
+                                <img src={require('../../images/HEY_DJ_06-995x560.jpg')} alt="" />
+                            </div>
+                            <div className="content">
+                                <div className="title">四大灣區</div>
+                                <div className="instruction">
+                                    <div>Lorem Ipsum dummy</div>
+                                    <div>Text of the printing industry.</div>
+                                    <div>2019</div>
+                                </div>
+                                <div className="bottom">
+                                    <div className="currentPage">01</div>
+                                    <div className="slash">/</div>
+                                    <div className="totalPages">15</div>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div className="imgDiv">
+                                <img src={require('../../images/SW_06-995x560.jpg')} alt="" />
+                            </div>
+                            <div className="content">
+                                <div className="title">四大灣區</div>
+                                <div className="instruction">
+                                    <div>Lorem Ipsum dummy</div>
+                                    <div>Text of the printing industry.</div>
+                                    <div>2019</div>
+                                </div>
+                                <div className="bottom">
+                                    <div className="currentPage">01</div>
+                                    <div className="slash">/</div>
+                                    <div className="totalPages">15</div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
 
             </div>
         )
     }
 }
 
-export {Qwzs};
+export { Qwzs };
